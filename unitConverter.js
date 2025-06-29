333bb0d794721015d3c4535ce72943072ada3a3b
theagentpenguin <gunaseelanabcd@gmail.com>
2025-06-29 23:22:59 +0530

    small touches

--- unitConverter.js
+++ unitConverter.js
@@ -1,1 +1,10 @@
-console.log("Let's goo!!!!");
+
+
+function letsTest(){
+    let input1 = parseInt(document.getElementById("inp_length").value);
+    let input2 = parseInt(document.getElementById("inp_length1").value);
+    let output;
+    output = input1+input2; 
+    document.getElementById("output_field").innerText=output;
+}
+
